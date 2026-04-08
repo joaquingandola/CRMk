94d8f4212848ca13f8ee5fda59a8a5355e056e36
Joaquin Gandola <joaquingandola@gmail.com>
2026-04-08 01:17:50 -0300

    fix aerolinea controller

--- crm/src/main/java/com/koraiken/crm/controller/AcompananteController.java
+++ crm/src/main/java/com/koraiken/crm/controller/AcompananteController.java
@@ -1,4 +1,21 @@
 package com.koraiken.crm.controller;
 
+import com.koraiken.crm.dto.Acompanante.AcompananteResponseDTO;
+import com.koraiken.crm.service.AcompananteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/acompanantes")
+@RequiredArgsConstructor
 public class AcompananteController {
+
+    private final AcompananteService acompananteService;
+
+    @PostMapping
+    public ResponseEntity<AcompananteResponseDTO>
+
 }
--- crm/src/main/java/com/koraiken/crm/controller/AerolineaController.java
+++ crm/src/main/java/com/koraiken/crm/controller/AerolineaController.java
@@ -1,29 +1,30 @@
 package com.koraiken.crm.controller;
 
 
-import com.koraiken.crm.dto.Acompanante.AcompananteCreateDTO;
-import com.koraiken.crm.dto.Acompanante.AcompananteResponseDTO;
-import com.koraiken.crm.service.AcompananteService;
+import com.koraiken.crm.dto.Aerolinea.AerolineaCreateDTO;
+import com.koraiken.crm.dto.Aerolinea.AerolineaResponseDTO;
+import com.koraiken.crm.service.AerolineaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/acompanantes")
 @RequiredArgsConstructor
 public class AerolineaController {
-    private final AcompananteService acompananteService;
+    private final AerolineaService aerolineaService;
 
     @PostMapping
-    public ResponseEntity<AcompananteResponseDTO> crear(@RequestBody AcompananteCreateDTO dto) {
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(acompananteService.crearAcompanante(dto));
+    public ResponseEntity<AerolineaResponseDTO> crear(@RequestBody AerolineaCreateDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(aerolineaService.crearAeroliena(dto));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<AcompananteResponseDTO> buscarPorId(@PathVariable Long id) {
-        return ResponseEntity.ok(acompananteService.buscarPorId(id));
+    @GetMapping
+    public ResponseEntity<List<AerolineaResponseDTO>> listar() {
+        return ResponseEntity.ok(aerolineaService.listarTodas());
     }
+
 }
