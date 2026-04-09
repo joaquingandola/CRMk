fada0b9439adf6429d1f9e3418c9151a52811167
Joaquin Gandola <joaquingandola@gmail.com>
2026-04-09 17:40:57 -0300

    Update GlobalExceptionHandler.java

--- crm/src/main/java/com/koraiken/crm/exception/GlobalExceptionHandler.java
+++ crm/src/main/java/com/koraiken/crm/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.koraiken.crm.dto.Error.ErrorResponseDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    //404
     @ExceptionHandler({
             AcompananteNotFoundException.class,
             AerolineaNotFoundException.class,
@@ -19,14 +21,58 @@ public class GlobalExceptionHandler {
             UserNotFoundException.class,
             ViajeNotFoundException.class,
             ClienteNotFoundException.class
+            //faltaria un exception  de destino not found
     })
     public ResponseEntity<ErrorResponseDTO> handleNotFound(RuntimeException ex, HttpServletRequest request) {
         return build(HttpStatus.NOT_FOUND, ex.getMessage(), request);
     }
 
+    // 409 conflict
+    @ExceptionHandler({
+            ClienteExisteException.class,
+            CiudadYaExisteException.class
+    })
+    public ResponseEntity<ErrorResponseDTO> handleConflict(RuntimeException ex, HttpServletRequest request) {
+        return build(HttpStatus.CONFLICT, ex.getMessage(), request);
+    }
+
+    // 422 UNPROCESSABLE content
+    @ExceptionHandler({
+            ClienteConViajesActivosException.class,
+            ViajeTransicionInvalidaException.class,
+            DestinoFechaInvalidaException.class
+    })
+    public ResponseEntity<ErrorResponseDTO> handleUnprocessable(RuntimeException exception, HttpServletRequest request) {
+        return build(HttpStatus.UNPROCESSABLE_CONTENT, exception.getMessage(), request);
+    }
+
+    //400 BAD request
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponseDTO> handleBadRequest(
+            IllegalArgumentException ex, HttpServletRequest request) {
+        return build(HttpStatus.BAD_REQUEST, ex.getMessage(), request);
+    }
+
+    //si el enum del estado del viaje no matchea ningun valor
+    @ExceptionHandler(org.springframework.web.method.annotation.MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponseDTO> handleEnumInvalido(Exception ex, HttpServletRequest request) {
+        return build(HttpStatus.BAD_REQUEST, "Valor invalido para el parametro: " + ex.getMessage(), request);
+    }
+
+    // ==== 401 unauthorized
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponseDTO> handleBadCredentials(
+            BadCredentialsException ex, HttpServletRequest request) {
+        return build(HttpStatus.UNAUTHORIZED, "Credenciales Incorrectas", request);
+    }
+
+    //403 forbidden
 
 
+    //413 payload too large
 
+
+    // 500 fallback
 
     //m,etodo interno
     private ResponseEntity<ErrorResponseDTO> build(HttpStatus status, String mensaje, HttpServletRequest request) {
